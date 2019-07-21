4f077f0d96204dee15648918f5f778c493bce97d
Pardo7 <walterpardo7@gmail.com>
2019-07-21 16:02:45 -0700

    feat: Implemented getDecks api async storage deck retrieval logic

--- utils/api.js
+++ utils/api.js
@@ -1,17 +1,12 @@
 import { AsyncStorage } from "react-native";
+import { DECK_STORAGE_KEY, fetchDeckResults } from "./_deck";
 
 export function getDecks() {
-
+  return AsyncStorage.getItem(DECK_STORAGE_KEY).then(fetchDeckResults);
 }
 
-export function getDeck(id) {
+export function getDeck(id) {}
 
-}
+export function saveDeckTitle(title) {}
 
-export function saveDeckTitle(title) {
-
-}
-
-export function addCardToDeck(title, card) {
-
-}
+export function addCardToDeck(title, card) {}
